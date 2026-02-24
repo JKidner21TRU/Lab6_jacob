090b10c0d936149ce39ce99e3a72660953c1a9f8
pacifique <kwizerapacy26@gmail.com>
2026-02-24 11:24:54 -0800

    This is a new commit upgrade of Git_collaboration added new a return with added name

--- app/src/main/java/com/example/lab6_jacob/DIsplayMe.java
+++ app/src/main/java/com/example/lab6_jacob/DIsplayMe.java
@@ -3,7 +3,8 @@ package com.example.lab6_jacob;
 public class DIsplayMe {
     public String displayMe()
     {
-        return "Pacifique_kwizera";
+        String name = "New changes in android studio.";
+        return "Pacifique_kwizera" + name;
         // THIs lab allowed to learn more about github collaboration and debug UI interface usinf Expross record.
     }
 }
