2cae248422aed9f045544f850ccbccb834a44e1e
kp3030 <149716127+kp3030@users.noreply.github.com>
2026-02-24 11:13:49 -0800

    Add comment about lab objectives in DIsplayMe.java
    Added a comment explaining the purpose of the lab.

--- app/src/main/java/com/example/lab6_jacob/DIsplayMe.java
+++ app/src/main/java/com/example/lab6_jacob/DIsplayMe.java
@@ -4,5 +4,6 @@ public class DIsplayMe {
     public String displayMe()
     {
         return "Pacifique_kwizera";
+        // THIs lab allowed to learn more about github collaboration and debug UI interface usinf Expross record.
     }
 }
